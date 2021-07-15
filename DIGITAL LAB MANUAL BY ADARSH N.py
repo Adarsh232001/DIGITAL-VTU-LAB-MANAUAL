71b438dd48c4c18694ad8f2abc0bc9b42da435f2
ADARSH N <70787887+Adarsh232001@users.noreply.github.com>
2021-07-15 22:46:10 +0530

    Update DIGITAL LAB MANUAL BY ADARSH N.py

--- DIGITAL LAB MANUAL BY ADARSH N.py
+++ DIGITAL LAB MANUAL BY ADARSH N.py
@@ -1,9 +1,12 @@
-import sys,os,pyperclip,webbrowser
+import sys,os,webbrowser
 from PyQt5 import QtCore
 from PyQt5 import QtWidgets
 from PyQt5 import QtGui
-from PyQt5.QtWidgets import QApplication, QWidget, QComboBox
-from PyQt5.QtGui import QStandardItemModel, QStandardItem, QFont, QTextOption
+from PyQt5.QtWidgets import QApplication, QMessageBox, QWidget, QComboBox
+from PyQt5.QtGui import  QStandardItemModel, QStandardItem, QFont, QTextOption
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
 
 data = {
     'C PROGRAMMING LABORATORY(18CPL17/27)': ["1.Basics of C","2.Commercial Calculator","3.Roots of Quadratic Equation",
@@ -21,39 +24,52 @@ data = {
                                 "5.GSM","6.CDMA","7.CRC-CCITT","8.Bellman-ford algorithm","9.TCP/IP Socket",
                                 "10.Datagram Socket","11.RSA Algorithm","12.Leaky Bucket Algorithm"],
     'DBMS LABORATORY WITH MINI PROJECT(18CSL58)' : ["1.Library Database","2.Order Database","3.Movie Database","4.College Database",
-                                "5.Company Database","DBMS MINI PROJECT"]
+                                "5.Company Database"],
+    'WEB TECHNOLOGY AND ITS APPLICAIONS(18CS63)' : ["1.Simple calculator in javascript","2.Squares and cubes in javascript","3.Text-growing and shrinking in javascript","4.String operations in javascript",
+                                "5.Student data in XML","6.Number of visitors in php","7.Digital clock in php","8-A.Calculator operations in php","8-B.Transpose of matrix in php","8-C.Multiplication of two matrix","8-D.Addition of two matrix",
+                                "9.Pattern matching with php","10.Student database with php and mysql"],
+    'COMPUTER GRAPHICS LABORATORY(18CSL67)' : ["1.Brenham's line drawing algorithm","2.Triangle rotation","3.colour cube and spin","4.color cube with perspective viewing",
+                                "5.Cohen-sutherland algorithm","6.Tea pot on a table","7.Sierpinski gasket","8.Bezier curve algorithm","9.Scan line algorithm"]
+
 }
 program_data = {
     'C PROGRAMMING LABORATORY(18CPL17/27)': ['LAB1.txt', 'LAB2.c', 'LAB3.c', 'LAB4.c', 'LAB5.c', 'LAB6.c', 'LAB7.c', 'LAB8.c', 'LAB9.c', 'LAB10.c', 'LAB11.c', 'LAB12.c', 'LAB13.c', 'LAB14.c', 'LAB15.c',],
     'DATA STRUCTURES LABORATORY(18CSL38)' : ['prog1.c','prog2.c','prog3.c','prog4.c','prog5.c','prog6.c','prog7.c','prog8.c','prog9.c','prog10.c','prog11.c','prog12.c'],
     'DESIGN AND ANALYSIS OF ALGORITHM LABORATORY(18CSL47)': ['prog1.java', 'prog2.java', 'prog3.java', 'prog4.java', 'prog5.java', 'prog6.java', 'prog7.java', 'prog8.java', 'prog9.java', 'prog10.java', 'prog11.java', 'prog12.java'],
     'COMPUTER NETWORK LABORATORY(18CSL57)': ['prog1.txt', 'prog2.txt', 'prog3.txt', 'prog4.txt', 'prog5.txt', 'prog6.txt', 'prog7.java', 'prog8.java', 'prog9.txt', 'prog10.txt', 'prog11.java', 'prog12.java'],
-    'DBMS LABORATORY WITH MINI PROJECT(18CSL58)': ['0.txt','1.txt','2.txt','3.txt','4.txt']
-
+    'DBMS LABORATORY WITH MINI PROJECT(18CSL58)': ['0.txt','1.txt','2.txt','3.txt','4.txt'],
+    'WEB TECHNOLOGY AND ITS APPLICAIONS(18CS63)' : ['1.txt', '2.txt', '3.txt', '4.txt', '5.xml', '6.php', '7.php', '8_A.php', '8_B.php', '8_C.php', '8_D.php', '9.php', '10.php'],
+    'COMPUTER GRAPHICS LABORATORY(18CSL67)' : ['PRO1.txt', 'PRO2.txt', 'PRO3.txt', 'PRO4.txt', 'PRO5.txt', 'PRO6.txt', 'PRO7.txt', 'PRO8.txt', 'PRO9.txt']
 }
 program_path = {
     'C PROGRAMMING LABORATORY(18CPL17/27)' : 'data\c\source',
     'DATA STRUCTURES LABORATORY(18CSL38)' : 'data\ds\source',
     'DESIGN AND ANALYSIS OF ALGORITHM LABORATORY(18CSL47)' : 'data\daa\source',
     'COMPUTER NETWORK LABORATORY(18CSL57)' : 'data\cns\source',
-    'DBMS LABORATORY WITH MINI PROJECT(18CSL58)' : 'data\dbms\source'
+    'DBMS LABORATORY WITH MINI PROJECT(18CSL58)' : 'data\dbms\source',
+    'WEB TECHNOLOGY AND ITS APPLICAIONS(18CS63)' : 'data\web\source',
+    'COMPUTER GRAPHICS LABORATORY(18CSL67)' : 'data\cgv\source'
 }
 image_path = {
     'C PROGRAMMING LABORATORY(18CPL17/27)' : 'data\c\output',
     'DATA STRUCTURES LABORATORY(18CSL38)' : 'data\ds\output',
     'DESIGN AND ANALYSIS OF ALGORITHM LABORATORY(18CSL47)' : 'data\daa\output',
     'COMPUTER NETWORK LABORATORY(18CSL57)' : 'data\cns\output',
-    'DBMS LABORATORY WITH MINI PROJECT(18CSL58)' : 'data\dbms\output'
+    'DBMS LABORATORY WITH MINI PROJECT(18CSL58)' : 'data\dbms\output',
+    'WEB TECHNOLOGY AND ITS APPLICAIONS(18CS63)' : 'data\web\output',
+    'COMPUTER GRAPHICS LABORATORY(18CSL67)' : 'data\cgv\output'
 }
 image_data = {
     'C PROGRAMMING LABORATORY(18CPL17/27)' : ['0','0.png', '1.png', '2.png', '3.png', '4.png', '5.png', '6.png', '7.png', '8.png', '9.png', '10.png', '11.png', '12.png', '13.png'],
     'DATA STRUCTURES LABORATORY(18CSL38)' : [],
     'DESIGN AND ANALYSIS OF ALGORITHM LABORATORY(18CSL47)' : ['1.jpg', '2.jpg', '3.jpg', '4.png', '5.png', '6.jpg', '7.png', '8.png', '9.png', '10.jpg', '11.png', '12.png'],
     'COMPUTER NETWORK LABORATORY(18CSL57)' : ['1.JPG', '2.JPG', '3.png', '4.JPG', '5.JPG', '6.jpg', '7.jpg', '8.jpg', '9.jpg', '10.jpg', '11.jpg', '12.jpg'],
-    'DBMS LABORATORY WITH MINI PROJECT(18CSL58)' : ['out0.txt', 'out1.txt', 'out2.txt', 'out3.txt', 'out4.txt']
+    'DBMS LABORATORY WITH MINI PROJECT(18CSL58)' : ['out0.txt', 'out1.txt', 'out2.txt', 'out3.txt', 'out4.txt'],
+    'WEB TECHNOLOGY AND ITS APPLICAIONS(18CS63)' : ['0.png', '1.png', '3.png', '4.png', '5.png', '6.png', '7.png', '8_A.png', '8_B.png', '8_C.png', '8_D.png', '9.png', '10.png'],
+    'COMPUTER GRAPHICS LABORATORY(18CSL67)' : ['Pro-1.gif', 'Pro-2.gif', 'Pro-3.gif', 'Pro-4.gif', 'Pro-5.jpg', 'Pro-6.jpg', 'Pro-7.gif', 'Pro-8.gif', 'Pro-9.gif']
 }
-text = ''
 program = []
+option_data = {}
 class AppDemo(QWidget):
     def __init__(self):
         super().__init__()
@@ -88,7 +104,7 @@ class AppDemo(QWidget):
         self.course_label.setFont(font)
         self.course_label.setText("SELECT THE COURSE:-")
         self.comboCourse = QComboBox(self.frame)
-        self.comboCourse.setGeometry(QtCore.QRect(240, 180, 411, 31))
+        self.comboCourse.setGeometry(QtCore.QRect(140, 180, 550, 31))
         self.comboCourse.setFont(QFont('', 12))
         self.comboCourse.setModel(self.model)
         self.subject_label = QtWidgets.QLabel(self.frame)
@@ -133,10 +149,12 @@ class AppDemo(QWidget):
                 subject = QStandardItem(value)
                 course.appendRow(subject)
         self.comboCourse.currentIndexChanged.connect(self.updateCourseCombo)
+        self.comboSubject.currentIndexChanged.connect(self.updateSubject)
         self.updateCourseCombo(0)
         self.setWindowTitle("VTU 2018 SCHEME DIGITAL LAB MANUAL BY ADARSH N")
-        self.setWindowFlag(QtCore.Qt.WindowMinimizeButtonHint, True)
-        self.setWindowFlag(QtCore.Qt.WindowMaximizeButtonHint, True)
+        scriptDir = os.path.dirname(os.path.realpath(__file__))
+        self.setWindowIcon(QtGui.QIcon(scriptDir + os.path.sep + 'data\icon.ico'))
+
 
     def updateCourseCombo(self, index):
         indx = self.model.index(index, 0, self.comboCourse.rootModelIndex())
@@ -144,12 +162,20 @@ class AppDemo(QWidget):
         self.comboSubject.setCurrentIndex(0)
         program.clear()
 
+    def updateSubject(self):
+        if len(program) > 0:
+            program.clear()
+            option_data.pop('course')
+            option_data.pop('subject')
+       
     def openlink(self):
         webbrowser.open('https://github.com/Adarsh232001/DIGITAL-VTU-LAB-MANAUAL')
 
     def openprogram(self):
         course = self.comboCourse.currentText()
+        option_data.update({'course': course})
         program_name = self.comboSubject.currentText()
+        option_data.update({'subject': program_name})
         Dialog = QtWidgets.QDialog()
         ui = Ui_Dialog()
         ui.setupUi(Dialog, course, program_name)
@@ -157,10 +183,12 @@ class AppDemo(QWidget):
 
         
 
-class Ui_Dialog(object):
+class Ui_Dialog(QWidget):
     def setupUi(self, Dialog, course, program_name):
         Dialog.setObjectName("Dialog")
         Dialog.resize(800, 650)
+        self.setWindowIcon(QtGui.QIcon('icon.png'))
+        self.setWindowIconText('logo')
         self.verticalLayoutWidget = QtWidgets.QWidget(Dialog)
         self.verticalLayoutWidget.setGeometry(QtCore.QRect(0, 0, 801, 571))
         self.verticalLayoutWidget.setObjectName("verticalLayoutWidget")
@@ -195,16 +223,14 @@ class Ui_Dialog(object):
                                 for vlaues_1 in b:
                                     if k.index(values) == b.index(vlaues_1):
                                         program.append(vlaues_1)
-        for i,k in data.items():
-            if i == course:
-                for values in k:
-                    if values == program_name:
-                        for a,b in image_data.items():
-                            if a == i:
-                                for vlaues_1 in b:
-                                    if k.index(values) == b.index(vlaues_1):
-                                        program.append(vlaues_1)                                                           
+
+                        for c,d in image_data.items():
+                            if c == i == course:
+                                for vlaues_2 in d:
+                                    if k.index(values) == d.index(vlaues_2):
+                                        program.append(vlaues_2)                
         code(self,program)
+
         
     def retranslateUi(self, Dialog):
         _translate = QtCore.QCoreApplication.translate
@@ -212,66 +238,94 @@ class Ui_Dialog(object):
         self.pushButton.setText(_translate("Dialog", "COPY TO CLIPBOARD"))
         self.pushButton_2.setText(_translate("Dialog", "OPEN SCREEN SHOT OF OUTPUT"))
 
+
     def copy_to_clipboard(self):
-        pyperclip.copy(text)
+        QApplication.clipboard().setText(name)
+        clipboard = QGuiApplication.clipboard()
+        clipboard.setText(name)
+        msg=QMessageBox()
+        msg.setText("copied on clipboard")
+        msg.exec_()
 
     def open_output(self):
-        Dialog_1 = QtWidgets.QDialog()
-        ui = Ui_Dialog_1()
-        ui.setupUi(Dialog_1)
-        Dialog_1.exec()
+        imagepath(self)
+        
 
 def code(self,program):
     for i,k in data.items():
         for value in k:
             for a,b in program_data.items():
-                if a == i:
+                if (a == i == option_data["course"]) and value == option_data["subject"]:
                     for value_1 in b:
                         for c,d in program_path.items():
                             if c == a and (program[0] == value_1 and k.index(value) == b.index(value_1)):
                                 filename = os.path.dirname(__file__) + "\\" + d + '\\' +program[0]
                                 if os.path.isfile(filename):
                                     text = open(filename).read()
+                                    global name 
+                                    name = text
                                     self.textBrowser.setText(text)
-                        
-
-class Ui_Dialog_1(object):
-    def setupUi(self, Dialog):
-        Dialog.setObjectName("Dialog")
-        Dialog.resize(800,650)
-        self.centralwidget = QtWidgets.QWidget(Dialog)
-        self.centralwidget.setObjectName("centralwidget")
-        self.retranslateUi(Dialog)
-        for i,k in data.items():
-            for value in k:
-                for a,b in image_data.items():
-                    if a == i:
-                        for value_1 in b:
-                            for c,d in image_path.items():
-                                if c == a and (program[1] == value_1 and k.index(value) == b.index(value_1)):
+                                                                                            
+def imagepath(self):
+    for i,k in data.items():
+                for value in k:
+                    for a,b in image_data.items():
+                        if (a == i == option_data["course"]) and (value == option_data["subject"]):
+                            for value_1 in b:
+                                for c,d in image_path.items():
+                                    if (c == a == option_data["course"]) and ((program[1] == value_1) and (k.index(value) == b.index(value_1))):
                                         filename = os.path.dirname(__file__) + "\\" + d + '\\' + program[1]
-                                        if filename.endswith('.txt'):
-                                            Dialog_2 = QtWidgets.QDialog()
-                                            ui = Ui_Dialog_2()
-                                            ui.setupUi(Dialog_2)
-                                            Dialog_2.exec()
-                                        elif os.path.isfile(filename):
-                                            self.photo = QtWidgets.QLabel(self.centralwidget)
-                                            self.photo.setSizePolicy(QtWidgets.QSizePolicy.Ignored, QtWidgets.QSizePolicy.Ignored)
-                                            self.photo.setGeometry(QtCore.QRect(0, 0, 800, 650))
-                                            self.photo.setText("")
-                                            self.photo.setPixmap(QtGui.QPixmap(filename))
-                                            self.photo.setScaledContents(True)
-                                            self.photo.setObjectName("photo")
-                                            
+                                        Dialog_1 = QtWidgets.QDialog()
+                                        ui = Ui_Dialog_1()
+                                        ui.setupUi(Dialog_1, filename)
+                                        Dialog_1.exec()
 
-        QtCore.QMetaObject.connectSlotsByName(Dialog)
+class Ui_Dialog_1(QWidget):
+    def setupUi(self, Dialog, filename):
+                                        
+        if filename.endswith('.txt'):
+            Dialog_2 = QtWidgets.QDialog()
+            ui = Ui_Dialog_2()
+            ui.setupUi(Dialog_2)
+            Dialog_2.exec()
+
+        elif filename.endswith('.gif'):
+            Dialog.setObjectName("Dialog")
+            Dialog.resize(1200, 700)
+            self.centralwidget = QtWidgets.QWidget(Dialog)
+            self.centralwidget.setObjectName("centralwidget")
+            self.label = QtWidgets.QLabel(self.centralwidget)
+            self.label.setGeometry(QtCore.QRect(0, 0, 1200, 700))
+            self.label.setObjectName("label")
+            self.movie = QMovie(filename)
+            self.label.setMovie(self.movie)
+            self.movie.start()
+    
+
+        elif os.path.isfile(filename):
+            Dialog.setObjectName("Dialog")
+            Dialog.resize(800,650)
+            self.centralwidget = QtWidgets.QWidget(Dialog)
+            self.centralwidget.setObjectName("centralwidget")
+            self.retranslateUi(Dialog)
+            self.photo = QtWidgets.QLabel(self.centralwidget)
+            self.photo.setSizePolicy(QtWidgets.QSizePolicy.Ignored, QtWidgets.QSizePolicy.Ignored)
+            self.photo.setGeometry(QtCore.QRect(0, 0, 800, 650))
+            self.photo.setText("")
+            self.photo.setPixmap(QtGui.QPixmap(filename))
+            self.photo.setScaledContents(True)
+            self.photo.setObjectName("photo")
+            scriptDir = os.path.dirname(os.path.realpath(__file__))
+            self.setWindowIcon(QtGui.QIcon(scriptDir + os.path.sep + 'icon.png'))                                          
+
+            QtCore.QMetaObject.connectSlotsByName(Dialog)
 
     def retranslateUi(self, Dialog):
         _translate = QtCore.QCoreApplication.translate
         Dialog.setWindowTitle(_translate("Dialog", "SCREEN SHOT DESIGNED AND DEVELOPED BY ADARSH N"))
 
-class Ui_Dialog_2(object):
+
+class Ui_Dialog_2(QWidget):
     def setupUi(self, Dialog_2):
         Dialog_2.setObjectName("Dialog_2")
         Dialog_2.resize(800, 650)
@@ -291,6 +345,8 @@ class Ui_Dialog_2(object):
                                         self.textBrowser.setText(text)
 
         self.retranslateUi(Dialog_2)
+        scriptDir = os.path.dirname(os.path.realpath(__file__))
+        self.setWindowIcon(QtGui.QIcon(scriptDir + os.path.sep + 'icon.png'))
         QtCore.QMetaObject.connectSlotsByName(Dialog_2)
 
     def retranslateUi(self, Dialog_2):
